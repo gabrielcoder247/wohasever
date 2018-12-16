621611ce0e1cf0206827deaa01b56fbecfa8c115
gabrielcoder <gabrielcoder247@gmail.com>
2018-12-16 18:33:43 +0300

    create url for home_page

--- wohasever/settings.py
+++ wohasever/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'wohasever.apps.WohaseverConfig',
+    'wohaseverapp.apps.WohaseverappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
