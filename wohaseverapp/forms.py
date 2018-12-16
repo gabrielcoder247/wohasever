3956f897cd0f64836db2ed8af4b10d68590bddf6
gabrielcoder <gabrielcoder247@gmail.com>
2018-12-16 19:02:54 +0300

    create form

---
+++ wohaseverapp/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import *
+
+# Create a signup form fields
+class SignUpForm(UserCreationForm):
+    name = forms.CharField(max_length=30,  required=False, help_text='Optional.')
+    username= forms.CharField(max_length=30,required=False, help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+  
+
+    class Meta:
+        model = User
+        fields = ('username', 'name', 'email',
+                  'password1', 'password2')
+
+# Create a profile form fields
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user','followers','following'] 
+     
+     
+# Create a image form fields
+class QuestionForm(forms.ModelForm):
+	class Meta:
+		model = Question
+        # exclude = ['user','location'],
+		fields = ['title', 'your_question', 'image_path','pub_date']
